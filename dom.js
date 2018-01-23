ded1eeb97042b36979329de0de57dd4e4d4e37f4
Cj Jordan <cj.jordan.tech@gmail.com>
2018-01-23 11:35:56 -0600

    initial commit

---
+++ dom.js
@@ -0,0 +1,55 @@
+var $ = function(sel) {
+    var elem = document.querySelectorAll(sel);
+
+    var text = function(str) {
+        for (var i = 0; i < elem.length; i++) {
+            elem[i].innerText = str;
+        }
+    }
+
+    var addClass = function(className) {
+        for (var i = 0; i < elem.length; i++) {
+            elem[i].classList.add(className);
+        }
+    }
+
+    var removeClass = function(className) {
+        for (var i = 0; i < elem.length; i++) {
+            elem[i].classList.remove(className);
+        }
+    }
+
+    var on = function(action, cb) {
+        for (var i = 0; i < elem.length; i++) {
+            elem[i].addEventListener(action, cb);
+        }
+    }
+
+    var toggleClass = function(className) {
+        for (var i = 0; i < elem.length; i++) {
+            if (elem[i].className.includes(className)) {
+                elem[i].classList.remove(className);
+            } else {
+                elem[i].classList.add(className);
+            }
+        }
+    }
+
+
+    var html = function(str) {
+        for (var i = 0; i < elem.length; i++) {
+            elem[i].innerHTML = str;
+        }
+    }
+
+    var publicAPI = {
+        element: elem,
+        text: text,
+        addClass: addClass,
+        removeClass: removeClass,
+        on: on,
+        html: html
+    }
+
+    return publicAPI;
+}
